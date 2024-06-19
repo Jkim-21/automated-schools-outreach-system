c3098fab29087506d92e44dec4d4071ff6f3ceba
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2024-06-19 11:02:53 -0400

    Move the spider scraper to start page browser

--- crawler/scrapy_crawler/spiders/google_spider.py
+++ crawler/scrapy_crawler/spiders/google_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-from scrapy_splash import SplashRequest, SlotPolicy
+from scrapy_splash import SplashRequest
 import json
 
 
@@ -7,27 +7,35 @@ class GoogleSpider(scrapy.Spider):
     name = "google_spider"
     allowed_domains = ["google.com"]
     
-    def start_requests(self):
-        search_queries = [
-            "Belmont High School website",
-            "Lexington High School website",
-            "Winchester High School website"
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.search_queries = [
+            "Belmont High School massachusetts website",
+            "Lexington High School massachusetts website",
+            "Winchester High School massachusetts website"
         ]
-        
-        for query in search_queries:
-            url = f'https://www.google.com/search?q={query}'
+    
+    def start_requests(self):
+        for query in self.search_queries:
+            url = f'https://www.startpage.com/do/search?cmd=process_search&query={query}'
             yield SplashRequest(url,
                                 args = {'wait': 1})
             
 
     def parse(self, response):
-        filename = f'response.html'
+        filename = 'response.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
-        elements = response.css('span.BNeawe').getall()
-        self.logger.info(f"Found elements: {elements}")
-        
+            
+        elements = response.css('div.result')[:3]
+            
         for element in elements:
+            link = element.css('span.link-text::text').get()
+            main_link = ""
+            if link:
+                if 'http' in link:
+                    link = link[link.index('http'):]
+            
             yield {
-                'element': element
+                'link': link
             }
