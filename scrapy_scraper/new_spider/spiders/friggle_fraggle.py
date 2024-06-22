e265ce36085a5226c3699fdd5249c051cbc9a067
damienwiese <damienwiese@gmail.com>
2024-06-22 17:37:44 -0500

    new scraper prototypes

---
+++ scrapy_scraper/new_spider/spiders/friggle_fraggle.py
@@ -0,0 +1,76 @@
+import scrapy
+import re
+import datetime
+from scrapy.selector import Selector
+from urllib.parse import urlparse
+import logging
+
+#imported things:
+#re: regex or regular expressions used to find strings
+#
+#
+#
+
+class school_scraper(scrapy.Spider):
+    name = 'school_scraper'
+    
+    # SETUP:
+    # put urls that you want to scrape in links.txt
+    # clean output.JSON, does not need to be empty but should be emptied
+    #call with: scrapy crawl school_scraper -o output.json -a max_depth=1 -a show_links=True (True or true both work)
+
+    #
+    def __init__(self, max_depth=2, show_links=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.show_links = show_links in ['True','true',True]
+        self.start_urls = self.read_urls()
+        self.visited_urls = set()
+        self.max_depth = int(max_depth)
+        self.basedomains = [urlparse(url).netloc for url in self.start_urls]
+
+    #parse links in links.txt for __init__
+    def read_urls(self):
+        with open('links.txt', 'r') as f:
+            urls = [url.strip() for url in f.readlines() if url.strip()]
+        return urls
+
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.parse, meta={'depth': 0, 'base_url': url}) 
+
+    def parse(self, response):
+        current_depth = response.meta['depth']
+        if current_depth > self.max_depth:
+            return
+        
+        self.visited_urls.add(response.url)
+        
+        links = response.css('a::attr(href)').getall()
+
+        for link in links:
+            absolute_url = response.urljoin(link)
+            parsed_url = urlparse(absolute_url)
+            if parsed_url.netloc in self.basedomains and absolute_url not in self.visited_urls:
+                self.visited_urls.add(absolute_url)
+
+                if self.show_links:
+                    yield {
+                        'type': 'link',
+                        'url': absolute_url
+                    }
+                
+                yield scrapy.Request(absolute_url, callback=self.parse, meta={'depth': current_depth + 1, 'base_url': response.meta['base_url']}) #add meta for other
+
+        emails = re.findall(r'[-a-zA-Z0-9._]+@[-a-zA-Z0-9_]+.[a-zA-Z0-9_.]+', response.text, re.IGNORECASE)
+        
+        for email in set(emails):
+            yield {
+                'link': 'school',
+                'email': email
+            }
+        
+
+#different places / ways looking for emails:
+
+
--- scrapy_scraper/new_spider/spiders/scraper_final.py
+++ scrapy_scraper/new_spider/spiders/scraper_final.py
@@ -1,23 +1,42 @@
 import scrapy
 import re
+import datetime
+from scrapy.selector import Selector
+from urllib.parse import urlparse
+import logging
 
-class proto_two(scrapy.Spider):
-    name = 'proto_two'
+#imported things:
+#re: regex or regular expressions used to find strings
+#
+#
+#
+
+class scraper_final(scrapy.Spider):
+    name = 'scraper_final'
     
+    # SETUP:
+    # put urls that you want to scrape in links.txt
+    # clean output.JSON, does not need to be empty but should be emptied
+    #call with: scrapy crawl scraper_final -o output.json -a max_depth=1 
+
+    #
     def __init__(self, max_depth=2, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.start_urls = self.read_urls()
         self.visited_urls = set()
-        self.max_depth = int(max_depth) 
+        self.max_depth = int(max_depth)
+        self.basedomains = [urlparse(url).netloc for url in self.start_urls]
 
+    #parse links in links.txt for __init__
     def read_urls(self):
         with open('links.txt', 'r') as f:
             urls = [url.strip() for url in f.readlines() if url.strip()]
         return urls
 
+
     def start_requests(self):
         for url in self.start_urls:
-            yield scrapy.Request(url, callback=self.parse, meta={'depth': 0})
+            yield scrapy.Request(url, callback=self.parse, meta={'depth': 0, 'base_url': url}) 
 
     def parse(self, response):
         current_depth = response.meta['depth']
@@ -26,44 +45,29 @@ class proto_two(scrapy.Spider):
         
         self.visited_urls.add(response.url)
         
-        
         links = response.css('a::attr(href)').getall()
+
         for link in links:
             absolute_url = response.urljoin(link)
-            if absolute_url not in self.visited_urls:
+            parsed_url = urlparse(absolute_url)
+            if parsed_url.netloc in self.basedomains and absolute_url not in self.visited_urls:
                 self.visited_urls.add(absolute_url)
                 yield {
                     'type': 'link',
                     'url': absolute_url
                 }
-
-                #can check links here (with regular expressions) to see if they have links embedded before callback - dont need to redo all later then
-                    #if it matches the regex empressions pass to -> email set 
-                    #
-
-                yield response.follow(link, callback=self.parse, meta={'depth': current_depth + 1})
-
+                
+                yield scrapy.Request(absolute_url, callback=self.parse, meta={'depth': current_depth + 1, 'base_url': response.meta['base_url']}) #add meta for other
 
         emails = re.findall(r'[-a-zA-Z0-9._]+@[-a-zA-Z0-9_]+.[a-zA-Z0-9_.]+', response.text, re.IGNORECASE)
-        #see if there is a better response to check - via doc
-
-        email_links = response.css('a[href^=mailto]::attr(href)').getall()
-        for email_link in email_links:
-            emails.append(email_link.split(':')[1])
-
         
-
-        text_nodes = response.css('*::text').getall()
-        for text in text_nodes:
-            emails.extend(re.findall(r'[-a-zA-Z0-9._]+@[-a-zA-Z0-9_]+.[a-zA-Z0-9_.]+', text, re.IGNORECASE))
-
-
         for email in set(emails):
             yield {
                 'type': 'email',
                 'email': email
             }
+        
+
+#different places / ways looking for emails:
 
 
-
-#scrapy crawl proto_two -o output.json -a max_depth=1 
